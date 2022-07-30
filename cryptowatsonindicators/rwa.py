304013a398f663ce7dc9b36ca1c57b62055dbb68
Raul Truco <raultruco@gmail.com>
2022-07-31 00:37:35 +0100

    fix: import from helpers package with .

--- cryptowatsonindicators/rwa.py
+++ cryptowatsonindicators/rwa.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import nasdaqdatalink
-from helpers import data
+from .helpers import data
 
 def _rwa_logarithmic_function(x,a,b,c):
     return a*np.log(b+x) + c
